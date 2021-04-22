242954db15c03a1d0a25873bd3129479fc4a6d49
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2021-04-22 14:27:51 +0200

    implemented configuration manager

---
+++ src/test/java/config/ConfigurationManagerTest.java
@@ -0,0 +1,13 @@
+package config;
+import org.junit.jupiter.api.*;
+
+class ConfigurationManagerTest {
+
+    ConfigurationManager config = new ConfigurationManager();
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void getMessages() {
+//        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+}
