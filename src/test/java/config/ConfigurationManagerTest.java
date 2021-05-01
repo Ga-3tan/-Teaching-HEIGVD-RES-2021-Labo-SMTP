dccbd4f2ea1ff7cb2d2e6a5556fb9161f4707cd5
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2021-05-01 15:46:41 +0200

    fixed path setter for config folder

--- src/test/java/config/ConfigurationManagerTest.java
+++ src/test/java/config/ConfigurationManagerTest.java
@@ -1,7 +1,14 @@
 package config;
 
+import org.junit.jupiter.api.Test;
+
 class ConfigurationManagerTest {
 
     ConfigurationManager config = new ConfigurationManager();
+
+    @Test
+    public void configurationManagerShouldHaveTheCorrectSmtpServerAddressSmtpServerPortNumberOfGroupWitnessesToCCFromTheConfigPropertiesFile() {
+
+    }
 
 }
